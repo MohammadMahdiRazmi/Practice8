7ba7262c8805cf42f2b43f17589e98e27c6919e3
MahdiRazmi <109846619+MohammadMahdiRazmi@users.noreply.github.com>
2023-08-18 15:08:18 +0330

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,11 @@
+import random
+namha = ["ali","reza","mahdi","sara"]
+halatha= ["shad","khandan","narahat","asabani"]
+fealha = ["ast","nist","bood","shood"]
+nam = random.choice(namha)
+haalat = random.choice(halatha)
+feal = random.choice(fealha)
+
+jomle = f"{nam} {haalat} {feal}"
+print(jomle)
+
